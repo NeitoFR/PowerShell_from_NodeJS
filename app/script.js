b9d1155c0185d2876afacddfa3003754a54edcf1
Willem HOUM <212682057@ge.com>
2019-06-27 15:18:18 +0200

    added comment

--- app/script.js
+++ app/script.js
@@ -1,29 +1,24 @@
-function httpGetAsync(theUrl, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-      callback(xmlHttp.responseText);
-  };
-  xmlHttp.open("GET", theUrl, true); // true for asynchronous
-  xmlHttp.send(null);
-}
-
 $(function() {
   console.log("jQuery Ready");
   var form = {};
+  // Cette fonction s'execute a chaque fois qu'on clique sur le bouton
   $("#btn").click(() => {
     console.log("Button clicked");
+    // On récupère le contenu des cases du formulaire
     form = {
       name: $("input[name=name]").val(),
       age: $("input[name=age]").val(),
       mail: $("input[name=mail]").val()
     };
     console.log(form);
+    // On envoi une requere HTTP POST au serveur sur la route /execPowerShellScript, avec la variable form qui contient toutes les infos du formulaire
     axios
       .post("/execPowerShellScript", form)
+      // Et on log la reponse (qui doit contenir l'output du script PowerShell executé)
       .then(function(response) {
-        console.log(response);
+        console.log(response.data);
       })
+      // Si y a des erreurs on les log
       .catch(function(error) {
         console.log(error);
       });
--- server/server.js
+++ server/server.js
@@ -16,21 +16,31 @@ app.get("/", function(req, res) {
   res.sendFile("index.html", { root: "./app" });
 });
 
+// Quand le serveur recois une requere HTTP POST sur la route /execPowerShellScript
 app.post("/execPowerShellScript", (req, res) => {
+  // req.body contient toute les infos qu'envoi le client (ici le name l'age et le mail)
     var form = req.body
     
-  console.log("Executing basic script with paramters", form);
+  console.log("Executing basic PowerShell script with paramters", form);
+  // Lancer le script Powershell en passant les paramètre
   child = spawn("powershell.exe",["-File", path.join(__dirname, "/script", "/example.ps1"), "-name", form.name, "-age", form.age, "-mail", form.mail]);
+  
+  // Récupérer ce que le script execute dans la variable output
   var output = ""
   child.stdout.on("data",function(data){
       output += data
   });
+
+  //Si il y a des erreurs on les log
   child.stderr.on("data",function(data){
       console.log("Powershell Errors: " + data);
   });
+
+  // Fin de l'execution du script
   child.on("exit",function(){
       console.log("Powershell Script ended");
-      console.log(output);      
+      console.log(output);
+      // on envoi au client la variable output      
       res.send(output).end();
   });
 
@@ -38,6 +48,7 @@ app.post("/execPowerShellScript", (req, res) => {
 
 const httpServer = http.createServer(app);
 
+// Le serveur écoute sur le port 5000 (peut etre changé)
 httpServer.listen(5000, () => {
   console.log("http://localhost:" + 5000);
 });
